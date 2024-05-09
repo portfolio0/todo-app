d118a57cde774b915a3c9229b974ec4c565d277b
portfolio0 <119784896+portfolio0@users.noreply.github.com>
2024-05-09 09:37:50 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -29,10 +29,10 @@ function Page() {
   let rendertask=<h2>No Task Available</h2>
   if(maintask.length>0){
   rendertask=maintask.map((t,i)=>{
-  return <li key={i} className='flex items-center justify-between bg-pink-300 mb-5 rounded text-lg font-semibold'>
+  return <li key={i} className='flex items-center justify-between bg-pink-300 mb-5 rounded text-nowrap'>
   <div className=' flex justify-around mb-5 w-2/3 pt-3' >
-    <h5 className=''>{t.title}</h5>
-      <h5>{t.desc}</h5>
+    <h5 className='mr-4 ml-4'>{t.title}</h5>
+      <h5 className='mr-4'>{t.desc}</h5>
   </div>
   <button onClick={deletehandler} className='bg-green-500 px-4 py-2 mr-36 rounded text-white'>Delete</button>
   </li>
@@ -41,8 +41,8 @@ function Page() {
   }
   return (
     <>
-      <h1 className='bg-black text-white p-5 text-2xl text-center'>ToDo List</h1>
-      <form onSubmit={handleSubmit} className='flex justify-center mt-10 mb-5 '>
+      <h1 className='bg-black text-white p-5 text-2xl text-center w-full'>ToDo List</h1>
+      <form onSubmit={handleSubmit} className='flex justify-center mt-10 mb-5 flex-wrap'>
         <input
           type='text'
           className='border-zinc-700 border-2 m-5 px-4 py-2'
